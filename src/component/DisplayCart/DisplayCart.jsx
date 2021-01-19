2a3f65d1276466a25a038d54af74c8bc29d09062
vismaya01 <kvvismaya98@gmail.com>
2021-01-19 22:45:53 +0530

    Added: customer details ui

--- src/component/DisplayCart/DisplayCart.jsx
+++ src/component/DisplayCart/DisplayCart.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DisplayCart.css'
 import Image from '../../assets/Image.png'
 import {Button} from '@material-ui/core'
+import Customer from '../Customer/Customer'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    MuiButtonRoot: {
+        height: 35,
+    },
+}));
 
 export default function DisplayCart() {
+    const classes = useStyles();
+    const [quantity, setQuantity] = useState(1)
+    const [open, setOpen] = useState(false)
+    const [openOrder, setOpenOrder] = useState(false)
+
     return (
         <div className="display-cart">
             <div className="cart1">
@@ -16,35 +29,42 @@ export default function DisplayCart() {
                     </div>
                     <div className="details1">
                             <div className="Booktitle">
-                                
+                                Don't Make Me Think
                             </div>
                             <div className="auther">
+                                by Steve Krug
                             </div>
                             <div className="cash">
-                                RS. 
+                                RS. 1500
                             </div>
                     </div>
                 </div>
                 <div className="quantity">
-                    <div className="control">
+                    <div className="control" onClick={() => setQuantity(quantity-1)}>
                         -
                     </div>
                     <div className="value1">
-
+                        {quantity}
                     </div>
-                    <div className="control">
+                    <div className="control" onClick={() => setQuantity(quantity+1)}>
                         +
                     </div>
                     <div className="remove">
                         Remove
                     </div>
                 </div>
-                <dis className="placeorder">
-                    <Button variant="contained" color="primary">PLACE ORDER</Button>
-                </dis>
+                <div className={open? "placeorder1" : "placeorder"}>
+                    <Button className={classes.MuiButtonRoot} onClick={() => setOpen(true)} variant="contained" color="primary">
+                        PLACE ORDER
+                    </Button>
+                </div>
             </div>
-            <div className="customer">
-                
+            <div className={open? "customer1" : "customer" }>
+                Customer Details
+                {open? <Customer open={openOrder} setOpen={setOpenOrder} /> : null }              
+            </div>
+            <div className={openOrder? "order1" : "order" }>
+                Order Details
             </div>
         </div>
     )
